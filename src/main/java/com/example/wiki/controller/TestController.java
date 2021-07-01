20cdf138015cbefaea953567ce0405ff29eb0be4
rickhuang <rick.liyue.huang@gmail.com>
2021-07-01 16:48:34 +1000

    feat: creat test.http in http directory to test interface

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class TestController {
 	 */
 	@RequestMapping("/hello")
 	public String hello() {
-		return "hello world";
+		return "Hello World!";
+	}
+
+	@PostMapping("/hello/post")
+	public String helloPost(String name) {
+		return "Hello World Post. " + name;
 	}
 }
